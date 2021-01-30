a1dfadaaa0fa62ce47cb4a211adeca51ceec28e7
Mekhail_Desktop <shagvaliev.mikhail@gmail.com>
2021-01-30 15:23:30 +0300

    add visualization module for optimization problems - move linear programming classes to separate file

--- lab1_linear_programming/main.py
+++ lab1_linear_programming/main.py
@@ -1,115 +1,7 @@
 import numpy as np
 import copy
-import scipy as sc
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-class LinearSystem:
-
-    def __init__(self, A=None, b=None):
-        if A is None:
-            A = []
-        if b is None:
-            b = []
-
-        self.A = np.array(A)
-        self.b = np.array(b)
-
-        if self.A.shape[0] != self.b.shape[0]:
-            raise ValueError('shapes of A and b dont match')
-
-    def col_num(self):
-        return self.A.shape[1]
-
-    def row_num(self):
-        return self.A.shape[0]
-
-    def select_A_columns(self, col_list):
-        return self.A[:, col_list].copy()
-
-    def foreach_A(self, func, inplace=True):
-        vec_func = np.vectorize(func)
-        res = vec_func(self.A.copy())
-
-        if inplace:
-            self.A = res
-
-        return LinearSystem(res, self.b.copy())
-
-    def foreach_b(self, func, inplace=True):
-        vec_func = np.vectorize(func)
-        res = vec_func(self.b.copy())
-
-        if inplace:
-            self.b = res
-
-        return LinearSystem(self.A.copy(), res)
-
-
-class LPProblem:
-
-    def __init__(self, x_size=0,
-                 LS_eq=None, LS_ineq=None, x_positive_indexes=None, c_objective=None):
-
-        if x_positive_indexes is None:
-            x_positive_indexes = []
-
-        if (LS_eq is not None and LS_eq.col_num() != x_size) or\
-                (LS_ineq is not None and LS_ineq.col_num() != x_size) or\
-                (c_objective is None or len(c_objective) != x_size):
-            raise ValueError('shape of x vector doesnt match system or objective vector')
-
-        self.LS_eq = LS_eq
-        self.LS_ineq = LS_ineq
-        self.x_positive_indexes = sorted(x_positive_indexes)
-        self.x_size = x_size
-        self.c_objective = np.array(c_objective)
-
-    def is_canonical(self):
-        return self.LS_ineq is None and self.x_size == len(self.x_positive_indexes)
-
-    def canonical(self, inplace=False):
-        if self.is_canonical():
-            return copy.deepcopy(self)
-
-        x_any_sign_indexes = list(set(range(self.x_size)) - set(self.x_positive_indexes))
-
-        A_m1_n1 = self.LS_ineq.select_A_columns(self.x_positive_indexes)
-        A_m1_n2 = self.LS_ineq.select_A_columns(x_any_sign_indexes)
-        neg_A_m1_n2 = A_m1_n2.copy() * -1
-        neg_E_m1_m1 = np.eye(self.LS_ineq.row_num()) * -1
-
-        A_m2_n1 = self.LS_eq.select_A_columns(self.x_positive_indexes)
-        A_m2_n2 = self.LS_eq.select_A_columns(x_any_sign_indexes)
-        neg_A_m2_n2 = A_m2_n2.copy() * -1
-        O_m2_m1 = np.zeros([self.LS_eq.row_num(), self.LS_ineq.row_num()])
-
-        A_upper = np.concatenate((A_m1_n1, A_m1_n2, neg_A_m1_n2, neg_E_m1_m1), axis=1)
-        A_lower = np.concatenate((A_m2_n1, A_m2_n2, neg_A_m2_n2, O_m2_m1), axis=1)
-
-        A = np.concatenate((A_upper, A_lower), axis=0)
-
-        b = np.concatenate((self.LS_ineq.b.copy(), self.LS_eq.b.copy()), axis=0)
-
-        c_n1 = self.c_objective[self.x_positive_indexes].copy()
-        c_n2 = self.c_objective[x_any_sign_indexes].copy()
-        neg_c_n2 = c_n2.copy() * -1
-        o_m1 = np.zeros(self.LS_ineq.row_num())
-
-        c = np.concatenate((c_n1, c_n2, neg_c_n2, o_m1), axis=0)
-
-        x_new_size = A.shape[1]
-        res = LPProblem(x_size=x_new_size,
-                        LS_eq=LinearSystem(A, b),
-                        LS_ineq=None,
-                        x_positive_indexes=list(range(x_new_size)),
-                        c_objective=c)
-
-        if inplace:
-            self.__dict__.update(res.__dict__)
-
-        return res
+from lab1_linear_programming.plot_function import visualize_func_in_2d_area
+from lab1_linear_programming.linear_programming import *
 
 
 if __name__ == '__main__':
@@ -129,5 +21,19 @@ if __name__ == '__main__':
                            [2, 3, 4, 1, 1]
                            )
     lp_problem.canonical(inplace=True)
+
+    lp_problem_2d = LPProblem(x_size=2,
+                              LS_eq=None,
+                              LS_ineq=LinearSystem(
+                                  [[-1, -1],
+                                   [-1, 1],
+                                   [2, 1]],
+                                  [-20, -20, -1]
+                              ),
+                              x_positive_indexes=[1],
+                              c_objective=[1, 1]
+                              )
+    visualize_func_in_2d_area(lp_problem_2d.objective_func(),
+                              lp_problem_2d.optimization_area_indicator())
 
     print('first lab')
---
+++ lab1_linear_programming/plot_function.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def visualize_func_in_2d_area(func, area_func_indicator=lambda x, eps: True,
+                              range_x=(-100, 100), range_y=(-100, 100), step=0.1, eps=0.0001):
+
+    def func_on_area_2d(x, y):
+        if area_func_indicator([x, y], eps):
+            return func([x, y])
+        else:
+            return np.nan
+
+    func_on_area_2d_vec = np.vectorize(func_on_area_2d)
+
+    X, Y = np.mgrid[range_x[0]:range_x[1]:step, range_y[0]:range_y[1]:step]
+    z = func_on_area_2d_vec(X, Y)
+    z_masked = np.ma.masked_where(np.isnan(z), z)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+
+    c = ax.pcolormesh(X, Y, z_masked)
+
+    plt.grid()
+    fig.colorbar(c)
+    plt.savefig('plots/x_plus_y.png')
+    plt.show()
