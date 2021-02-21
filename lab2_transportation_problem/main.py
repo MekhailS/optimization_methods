f27a7b339bde6cf6b6b8b24873de9282d94b297e
Ovechkin Danil <ovechkin.da@edu.spbstu.ru>
2021-02-21 21:47:34 +0300

    Add north-west method

--- lab2_transportation_problem/main.py
+++ lab2_transportation_problem/main.py
@@ -7,7 +7,14 @@ if __name__ == '__main__':
          [11, 1, 6, 7, 8, 8],
          [12, 1, 6, 7, 8, 8]]
 
-    transport_problem = TransportProblem(A)
+    A_Rodionova = \
+        [[0, 3, 6, 5, 6],
+         [7, 3, 11, 3, 10],
+         [4, 1, 9, 2, 8],
+         [9, 7, 4, 10, 5]]
+    transport_problem = TransportProblem(A_Rodionova)
+    transport_problem.northwest_corner_method()
+    print(transport_problem.obj_function_value())
     # if ~transport_problem.is_closed_problem():
     #     transport_problem.to_closed_problem()
 
--- lab2_transportation_problem/transportation_problem.py
+++ lab2_transportation_problem/transportation_problem.py
@@ -9,18 +9,18 @@ class TransportProblem:
         self.s_a = np.sum(self.rate_array, axis=1)[0]
         self.n = len(A) - 1  # кол-во пунктов хранения
         self.m = len(A[0]) - 1  # кол-во пунктов назначения
+        self.result_vec = []
 
         self.__create_supplies_array()
 
     def __create_supplies_array(self):
-        self.supplies_array = [[0 for j in range(self.m + 1)] for i in range(self.n + 1)]
+        self.supplies_array = np.array([[0 for j in range(self.m + 1)] for i in range(self.n + 1)])
 
         for j in range(1, self.m + 1):
-            self.supplies_array[0][j] = self.rate_array[0][j]
+            self.supplies_array[0][j] = self.rate_array[0][j] 
 
         for i in range(1, self.n + 1):
             self.supplies_array[i][0] = self.rate_array[i][0]
-
 
     def __is_closed_problem(self):
         return self.s_a == self.s_b
@@ -31,19 +31,46 @@ class TransportProblem:
             self.s_b = self.s_a
             self.rate_array = np.r_[self.rate_array, [[b_new] + [0 for i in range(self.m)]]]
 
-        elif self.s_a < self.s_b: # TODO: добавить штраф за недопоставку
+        elif self.s_a < self.s_b:  # TODO: добавить штраф за недопоставку
             a_new = self.s_b - self.s_a
             self.s_a = self.s_b
             self.rate_array = np.c_[self.rate_array, [a_new] + [0 for i in range(self.n)]]
 
-    def __northwest_corner_method(self):
-        print('northwest')
+    # создание вектора решение из таблицы
+    def __create_result_vector(self):
+        self.result_vec.clear()
+
+        for i in range(1, self.n + 1):
+            for j in range(1, self.m + 1):
+                self.result_vec.append(self.supplies_array[i][j])
+
+    # вычисление значения целевой функции
+    def obj_function_value(self):
+        s = 0
+
+        for i in range(1, self.n + 1):
+            for j in range(1, self.m + 1):
+                s += self.rate_array[i][j] * self.supplies_array[i][j]
+
+        return s
+
+    def northwest_corner_method(self):
+
+        def __cell_value(i, j):
+            min_el = np.minimum(self.supplies_array[i][0], self.supplies_array[0][j])
+            self.supplies_array[0][j] -= min_el
+            self.supplies_array[i][0] -= min_el
+            self.supplies_array[i][j] = min_el
+
+        for k in range(1, self.m):
+            __cell_value(k, k)
+            __cell_value(k, k + 1)
+
+        self.__create_result_vector()
+        print('DONE!')
 
     def __potential_method(self):
         print('potential')
 
-
     def __brute_force_method(self):
         print('brute_force')
-
-
