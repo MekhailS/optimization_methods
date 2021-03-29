working steepest gradient descent - fix biffonacchi - add call_count decorator - fix in function_differentiable

lab4_gradient_descent/function_differentiable.py
@@ -1,19 +1,36 @@
+import numpy as np
+
+from call_count import call_count
 
 
 class FunctionDifferentiable:
 
-    def __init__(self, func, gradient):
+    def __init__(self, func, gradient=None, hessian=None):
         """
         :param func: function with one argument as n-dimensional numpy array
         :param gradient: gradient of function 'func' with one argument as n-dimensional numpy array
         """
         self.__func = func
-        self.__gradient = func
+        self.__gradient = gradient
+        self.__hessian = hessian
 
-    @property
+    @call_count
     def ev_func(self, x):
+        x = np.asarray(x)
         return self.__func(x)
 
-    @property
+    @call_count
     def ev_gradient(self, x):
-        return self.__gradient(x)
+        if self.__gradient is None:
+            return None
+
+        x = np.asarray(x)
+        return np.asarray(self.__gradient(x))
+
+    @call_count
+    def ev_hessian(self, x):
+        if self.__hessian is None:
+            return None
+
+        x = np.asarray(x)
+        return np.asarray(self.__hessian(x))

lab4_gradient_descent/gradient_steepest_descent.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy import optimize as opt
+from fibonacchi import FibonacchiSolver
 
 
 class GradientSteepestDescent:
@@ -9,16 +11,26 @@ class GradientSteepestDescent:
 
     @property
     def __initial_x(self):
-        return np.zeros(self.__dim)
+        #return np.zeros(self.__dim)
+        return np.random.randn(self.__dim)
 
     def optimize(self, tol):
         n_iterations_func_not_changing = 0
-        LIMIT_ITERATIONS_FUNC_NOT_CHANGING = 5
+        LIMIT_ITERATIONS_FUNC_NOT_CHANGING = 10
 
-        def stopping_rule(x_cur, x_next):
+        def stopping_rule(x_cur, x_next, rule='df'):
             nonlocal n_iterations_func_not_changing
 
-            if np.abs(self.__func.ev_func(x_cur) - self.__func.ev_func(x_next)) < tol:
+            dict_rule = {
+                'df':
+                    lambda x_cur, x_next : np.abs(self.__func.ev_func(x_cur) - self.__func.ev_func(x_next)) ,
+                'dx':
+                    lambda x_cur, x_next : np.linalg.norm(x_cur - x_next, ord=2),
+                'grad_f':
+                    lambda x_cur, _ : np.linalg.norm(self.__func.ev_gradient(x_next), ord=2)
+            }
+
+            if dict_rule[rule](x_cur, x_next) <= tol:
                 n_iterations_func_not_changing += 1
             else:
                 n_iterations_func_not_changing = 0
@@ -30,15 +42,25 @@ class GradientSteepestDescent:
         x_history = []
         x_cur = self.__initial_x
         while True:
+            print(f'x is {x_cur}, function at x is {self.__func.ev_func(x_cur)}')
             x_history.append(x_cur)
 
+            grad_x_cur = self.__func.ev_gradient(x_cur)
             def func_after_step(step):
-                return self.__func(self.__func.ev_func(x_cur) - step * self.__func.ev_gradient(x_cur))
+                x_next = x_cur - step * grad_x_cur
+                return self.__func.ev_func(x_next)
 
-            step = 0.01
-            x_next = self.__func.ev_func(x_cur) - step * self.__func.ev_gradient(x_cur)
+            STEP_RANGE = [0, 1]
+            fib_solver = FibonacchiSolver(
+                func_after_step,
+                STEP_RANGE[0], STEP_RANGE[1],
+                result_interval_length=1.e-3,
+                eps=1.e-10
+            )
+            step = fib_solver.solve()
+            x_next = x_cur - step * grad_x_cur
 
-            if stopping_rule(x_cur, x_next):
+            if stopping_rule(x_cur, x_next, rule='grad_f'):
                 x_history.append(x_next)
                 return x_next, x_history
 