9cd8060ab0e9c8f0303bcea286bcad4ebd6010e2
OvechkinDanil <danil190600@gmail.com>
2021-03-30 22:57:48 +0300

    Change recursion to loop

--- lab4_gradient_descent/dfp_method.py
+++ lab4_gradient_descent/dfp_method.py
@@ -14,44 +14,30 @@ class DFP(IterativeOptimizer):
         self.__x_history = []
         self.__x_history.append(np.random.randn(dim))
 
-    def optimize(self, tol, print_info=True):
-        self.__termination_rule = IterativeOptimizer.TerminationRule(
-            optimizer=self,
-            limit_iterations_termination_true=1,
-            tol=tol,
-            rule_name='grad_f'
-        )
+        self.step = 'first'
 
-        result = self.__first_step()
-
-        if print_info:
-            print('DAVIDON-FLETCHER-POWELL METHOD:')
-            print(f'optimal solution is {result[0]}, function at x is {self._func.ev_func(result[0])}')
-
-            print('Step by step solution:')
-            for x in result[1]:
-                print(f'x is {x}, function at x is {self._func.ev_func(x)}')
-
-        return result
-
-    def __first_step(self):
-        if self.__termination_rule(0, self.__x_history[-1]):
-            return self.__x_history[-1], self.__x_history
-        else:
-            return self.__second_step()
+    # def __first_step(self):
+    #     if self.__termination_rule(0, self.__x_history[-1]):
+    #         return self.__x_history[-1], self.__x_history
+    #     else:
+    #         return self.__second_step()
 
     def __second_step(self):
         p_k = self.__A @ (-self._func.ev_gradient(self.__x_history[-1]))
 
         alpha_k = optimize.golden(lambda alpha: self._func.ev_func(self.__x_history[-1] + alpha * p_k))
+        print(f'alpha = {alpha_k}')
         self.__x_history.append(self.__x_history[-1] + alpha_k * p_k)
 
         if self.__cur_index == self._dim or (self.__cur_index / self._dim) % 2 == 0:
             self.__A = np.identity(self._dim)
             self.__cur_index += 1
-            return self.__first_step()
+
+            self.step = 'first'
+            # return self.__first_step()
         else:
-            return self.__third_step()
+            self.step = 'third'
+            # return self.__third_step()
 
     def __third_step(self):
         delta_x = np.array(self.__x_history[-1] - self.__x_history[-2])
@@ -67,4 +53,39 @@ class DFP(IterativeOptimizer):
         self.__A = self.__A - _B - _C
 
         self.__cur_index += 1
-        return self.__first_step()
+
+        self.step = 'first'
+        # return self.__first_step()
+
+    def optimize(self, tol, print_info=True):
+        self.__termination_rule = IterativeOptimizer.TerminationRule(
+            optimizer=self,
+            limit_iterations_termination_true=1,
+            tol=tol,
+            rule_name='grad_f'
+        )
+
+        # result = self.__first_step()
+        while True:
+            if self.step == 'first':
+                if self.__termination_rule(0, self.__x_history[-1]):
+                    result = self.__x_history[-1], self.__x_history
+                    break
+                else:
+                    self.step = 'second'
+
+            elif self.step == 'second':
+                self.__second_step()
+
+            elif self.step == 'third':
+                self.__third_step()
+
+        if print_info:
+            print('DAVIDON-FLETCHER-POWELL METHOD:')
+            print(f'optimal solution is {result[0]}, function at x is {self._func.ev_func(result[0])}')
+
+            print('Step by step solution:')
+            for x in result[1]:
+                print(f'x is {x}, function at x is {self._func.ev_func(x)}')
+
+        return result
